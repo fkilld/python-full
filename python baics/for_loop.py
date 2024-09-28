e751f45b55579e5ba60d60e604f7bc98edcd2ddc
samrudhi gaikwad <samrudhigaikwad2004@gmail.com>
2024-09-28 16:32:58 +0530

    Create for_loop.py
    save file

---
+++ python baics/for_loop.py
@@ -0,0 +1,19 @@
+numbers=[1,2,3,4,5,6,7,8,9,10]
+even=[]
+# for i in numbers:
+#     print(i)
+
+# print([i for i in numbers])
+# for i in numbers:
+#      if i%2==0:
+#          even.append(i)
+# print(even) 
+# [even.append(i) for i in numbers if i%2==0]
+# print(even)
+
+sum=[]
+for i in numbers:
+    n=i*2
+    sum.append(n)
+print(sum)
+#[sum.append(i) for i in numbers if i*3]
